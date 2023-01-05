5736553ef2f4ece7c8c5bd290a85559e8344f46b
Jerry Victor <jerryvictor@macbook-pro.myfiosgateway.com>
2023-01-04 22:26:13 -0500

    created controller for home and user table

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,2 +1,38 @@
 class HomesController < ApplicationController
+rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
+def index
+    homes = Home.allrender json: homes
+end
+
+def show
+    home = Home.find(params[:id])
+    render json: home, status: :created 
+end
+
+def create
+    home = Home.create!(home_params)
+    render json: home, status: :created 
+end
+
+def destroy
+    home = Home.find(params[:id])
+    home.destroy
+    head :no_content 
+end
+
+private 
+
+def home_params 
+    params.permit(:home_type, :total_occupancy, :total_bedrooms, :total_bathrooms, :summary, :address, :has_tv, :has_kitchen, :has_air_con, :has_heating, :has_internet, :price, :owner_id, :latitude, :longitude, title) 
+end
+
+def render_unprocessable_entity_response(exception)
+    render json: {errors: exception.record.errors.full_messages}, status: :unprocessable_entity
+end 
+
+def render_not_found_response
+    render json: {error:"House Nopt Found"}, status: :not_found  
+end
 end
--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,2 +1,38 @@
 class ImagesController < ApplicationController
+    rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+    
+    def index
+        images = image.allrender json: images
+    end
+    
+    def show
+        image = image.find(params[:id])
+        render json: image, status: :created 
+    end
+    
+    def create
+        image = image.create!(image_params)
+        render json: image, status: :created 
+    end
+    
+    def destroy
+        image = image.find(params[:id])
+        image.destroy
+        head :no_content 
+    end
+    
+    private 
+
+    def review_params
+        params.permit(:home_id, :room_type, :img)
+    end
+
+    def render_unprocessable_entity_response(exception)
+        render json: {errors: exception.record.errors.full_messages }, status: :unprocessable_entity
+    end
+
+    def render_not_found_response
+        render json: {error: "Image Not Found"}, status:  :not_found
+    end
 end
